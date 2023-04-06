caa0f7a2ee167520335c56f557383a08d2427131
Japinha Cheiroso <devwebpw@gmail.com>
2023-04-05 22:26:15 -0300

    É o commit

---
+++ ExeLuanII/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExeLuanII.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            ViewData["Email"] = "GoriladaAngola@gmail.com";
+              
+            return View();
+        }
+
+        public ActionResult Iis()
+        {
+            return View();
+        }
+    }
+}
